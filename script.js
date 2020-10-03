4d9453878a0de7f06e1ee15e2ed60f86a0e22799
Rachel Moore <moorer@gcsnc.com>
2020-10-03 09:26:35 -0400

    changed bknd colors for all time slots

--- script.js
+++ script.js
@@ -30,9 +30,9 @@ postDate()
   // midBox.forEach()
 
 
-
+// change background colors depending on time of day
 function change9() {
-  if (hour === 08);
+  if (hour === 09);
   document.getElementById('nine').classList.add('textAreaPresent');
   document.getElementById('ten').classList.add('textAreaFuture');
   document.getElementById('eleven').classList.add('textAreaFuture');
@@ -44,14 +44,41 @@ function change9() {
   document.getElementById('five').classList.add('textAreaFuture');
   document.getElementById('six').classList.add('textAreaFuture');
 }
-change9()
-
-function change10() {
-  if (hour === 10) 
+change9();
+// function change10() {
+//   if (hour === 10) 
+//   document.getElementById('nine').classList.add('textAreaPast');
+//   document.getElementById('ten').classList.add('textAreaPresent');
+//   document.getElementById('eleven').classList.add('textAreaFuture');
+//   document.getElementById('twelve').classList.add('textAreaFuture');
+//   document.getElementById('one').classList.add('textAreaFuture');
+//   document.getElementById('two').classList.add('textAreaFuture');
+//   document.getElementById('three').classList.add('textAreaFuture');
+//   document.getElementById('four').classList.add('textAreaFuture');
+//   document.getElementById('five').classList.add('textAreaFuture');
+//   document.getElementById('six').classList.add('textAreaFuture');
+// }
+// change10();
+// function change11() {
+//   if (hour === 11) 
+//   document.getElementById('nine').classList.add('textAreaPast');
+//   document.getElementById('ten').classList.add('textAreaPast');
+//   document.getElementById('eleven').classList.add('textAreaPresent');
+//   document.getElementById('twelve').classList.add('textAreaFuture');
+//   document.getElementById('one').classList.add('textAreaFuture');
+//   document.getElementById('two').classList.add('textAreaFuture');
+//   document.getElementById('three').classList.add('textAreaFuture');
+//   document.getElementById('four').classList.add('textAreaFuture');
+//   document.getElementById('five').classList.add('textAreaFuture');
+//   document.getElementById('six').classList.add('textAreaFuture');
+// }
+change11();
+function change12() {
+  if (hour === 12) 
   document.getElementById('nine').classList.add('textAreaPast');
-  document.getElementById('ten').classList.add('textAreaPresent');
-  document.getElementById('eleven').classList.add('textAreaFuture');
-  document.getElementById('twelve').classList.add('textAreaFuture');
+  document.getElementById('ten').classList.add('textAreaPast');
+  document.getElementById('eleven').classList.add('textAreaPast');
+  document.getElementById('twelve').classList.add('textAreaPresent');
   document.getElementById('one').classList.add('textAreaFuture');
   document.getElementById('two').classList.add('textAreaFuture');
   document.getElementById('three').classList.add('textAreaFuture');
@@ -59,14 +86,90 @@ function change10() {
   document.getElementById('five').classList.add('textAreaFuture');
   document.getElementById('six').classList.add('textAreaFuture');
 }
-change10()
+change12();
+function change1() {
+  if (hour === 13) 
+  document.getElementById('nine').classList.add('textAreaPast');
+  document.getElementById('ten').classList.add('textAreaPast');
+  document.getElementById('eleven').classList.add('textAreaPast');
+  document.getElementById('twelve').classList.add('textAreaPast');
+  document.getElementById('one').classList.add('textAreaPresent');
+  document.getElementById('two').classList.add('textAreaFuture');
+  document.getElementById('three').classList.add('textAreaFuture');
+  document.getElementById('four').classList.add('textAreaFuture');
+  document.getElementById('five').classList.add('textAreaFuture');
+  document.getElementById('six').classList.add('textAreaFuture');
+}
+change1();
+function change2() {
+  if (hour === 14) 
+  document.getElementById('nine').classList.add('textAreaPast');
+  document.getElementById('ten').classList.add('textAreaPast');
+  document.getElementById('eleven').classList.add('textAreaPast');
+  document.getElementById('twelve').classList.add('textAreaPast');
+  document.getElementById('one').classList.add('textAreaPast');
+  document.getElementById('two').classList.add('textAreaPresent');
+  document.getElementById('three').classList.add('textAreaFuture');
+  document.getElementById('four').classList.add('textAreaFuture');
+  document.getElementById('five').classList.add('textAreaFuture');
+  document.getElementById('six').classList.add('textAreaFuture');
+}
+change2();
+function change3() {
+  if (hour === 15) 
+  document.getElementById('nine').classList.add('textAreaPast');
+  document.getElementById('ten').classList.add('textAreaPast');
+  document.getElementById('eleven').classList.add('textAreaPast');
+  document.getElementById('twelve').classList.add('textAreaPast');
+  document.getElementById('one').classList.add('textAreaPast');
+  document.getElementById('two').classList.add('textAreaPast');
+  document.getElementById('three').classList.add('textAreaPresent');
+  document.getElementById('four').classList.add('textAreaFuture');
+  document.getElementById('five').classList.add('textAreaFuture');
+  document.getElementById('six').classList.add('textAreaFuture');
+}
+change3();
+function change4() {
+  if (hour === 16) 
+  document.getElementById('nine').classList.add('textAreaPast');
+  document.getElementById('ten').classList.add('textAreaPast');
+  document.getElementById('eleven').classList.add('textAreaPast');
+  document.getElementById('twelve').classList.add('textAreaPast');
+  document.getElementById('one').classList.add('textAreaPast');
+  document.getElementById('two').classList.add('textAreaPast');
+  document.getElementById('three').classList.add('textAreaPast');
+  document.getElementById('four').classList.add('textAreaPresent');
+  document.getElementById('five').classList.add('textAreaFuture');
+  document.getElementById('six').classList.add('textAreaFuture');
+}
+change4();
+function change5() {
+  if (hour === 17) 
+  document.getElementById('nine').classList.add('textAreaPast');
+  document.getElementById('ten').classList.add('textAreaPast');
+  document.getElementById('eleven').classList.add('textAreaPast');
+  document.getElementById('twelve').classList.add('textAreaPast');
+  document.getElementById('one').classList.add('textAreaPast');
+  document.getElementById('two').classList.add('textAreaPast');
+  document.getElementById('three').classList.add('textAreaPast');
+  document.getElementById('four').classList.add('textAreaPast');
+  document.getElementById('five').classList.add('textAreaPresent');
+  document.getElementById('six').classList.add('textAreaFuture');
+}
+change5();
+function change6() {
+  if (hour === 18) 
+  document.getElementById('nine').classList.add('textAreaPast');
+  document.getElementById('ten').classList.add('textAreaPast');
+  document.getElementById('eleven').classList.add('textAreaPast');
+  document.getElementById('twelve').classList.add('textAreaPast');
+  document.getElementById('one').classList.add('textAreaPast');
+  document.getElementById('two').classList.add('textAreaPast');
+  document.getElementById('three').classList.add('textAreaPast');
+  document.getElementById('four').classList.add('textAreaPast');
+  document.getElementById('five').classList.add('textAreaPast');
+  document.getElementById('six').classList.add('textAreaPresent');
+}
+change6();
 
 
-
-
-// function changebknd() {
-//   if (hour === 09)
-// } else { 
-//   (hour === 08)
-// }
-// changebknd()
